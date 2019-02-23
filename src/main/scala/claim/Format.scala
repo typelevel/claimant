f4ff5ac1c619e3e3684867b0cb3ba49786f1a337
Erik Osheim <d_m@plastic-idolatry.com>
2019-02-23 15:26:47 -0500

    Add a test for (more) recursive annotations.
    Also uncomment some commented tests.

--- src/main/scala/claim/Format.scala
+++ src/main/scala/claim/Format.scala
@@ -11,21 +11,21 @@ object Format {
 
   def str1(c: Context)(x: c.Tree, method: String, input: Option[c.Tree]): c.Tree = {
     import c.universe._
-    addInput(c)(q"""${x}.toString + "." + $method""", input)
+    addInput(c)(q"""$x.toString + "." + $method""", input)
   }
 
   def str2(c: Context)(x: c.Tree, op: String, y: c.Tree, input: Option[c.Tree]): c.Tree = {
     import c.universe._
-    addInput(c)(q"""${x}.toString + " " + $op + " " + ${y}.toString""", input)
+    addInput(c)(q"""$x.toString + " " + $op + " " + $y.toString""", input)
   }
 
   def str1_1(c: Context)(x: c.Tree, method: String, y: c.Tree, input: Option[c.Tree]): c.Tree = {
     import c.universe._
-    addInput(c)(q"""${x}.toString + "." + $method + "(" + ${y}.toString + ")"""", input)
+    addInput(c)(q"""$x.toString + "." + $method + "(" + $y.toString + ")"""", input)
   }
 
   def str1_2(c: Context)(o: c.Tree, method: String, x: c.Tree, y: c.Tree, input: Option[c.Tree]): c.Tree = {
     import c.universe._
-    addInput(c)(q"""${o}.toString + "." + $method + "(" + ${x}.toString + ", " + ${y}.toString + ")"""", input)
+    addInput(c)(q"""$o.toString + "." + $method + "(" + $x.toString + ", " + $y.toString + ")"""", input)
   }
 }
--- src/test/scala/claim/ClaimCatsTest.scala
+++ src/test/scala/claim/ClaimCatsTest.scala
@@ -18,42 +18,45 @@ object ClaimCatsTest extends Properties("ClaimCatsTest") {
 
   val (h1, h2) = (Height(1), Height(2))
 
-  // property("h1 === h2") =
-  //   test(Claim(h1 === h2), "falsified: Height(1) === Height(2)")
-  // 
-  // property("h1 !== h1") =
-  //   test(Claim(h1 =!= h1), "falsified: Height(1) =!= Height(1)")
-  // 
-  // property("(h1 compare h2) == 0") =
-  //   test(Claim((h1 compare h2) == 0), "falsified: OrderForHeight.compare(Height(1), Height(2)) {-1} == 0")
-  // 
-  // property("(h1 partialCompare h2) == 0.0") =
-  //   test(Claim((h1 partialCompare h2) == 0.0), "falsified: OrderForHeight.partialCompare(Height(1), Height(2)) {-1.0} == 0.0")
-  // 
-  // property("h1 < h1") =
-  //   test(Claim(h1 < h1), "falsified: Height(1) < Height(1)")
-  // 
-  // property("h2 <= h1") =
-  //   test(Claim(h2 <= h1), "falsified: Height(2) <= Height(1)")
-  // 
-  // property("h1 > h1") =
-  //   test(Claim(h1 > h1), "falsified: Height(1) > Height(1)")
-  // 
-  // property("h1 >= h2") =
-  //   test(Claim(h1 >= h2), "falsified: Height(1) >= Height(2)")
-  // 
-  // property("(1 pmin 2) = None") =
-  //   test(Claim((1 pmin 2) == None), "falsified: 1 pmin 2 {Some(1)} == None")
-  // 
-  // property("(1 pmax 2) = None") =
-  //   test(Claim((1 pmax 2) == None), "falsified: 1 pmax 2 {Some(2)} == None")
-  // 
-  // property("(1 min 2) = None") =
-  //   test(Claim((1 min 2) == 0), "falsified: 1 min 2 {Some(1)} == 0")
+  property("h1 === h2") =
+    test(Claim(h1 === h2), "falsified: Height(1) === Height(2)")
+
+  property("h1 !== h1") =
+    test(Claim(h1 =!= h1), "falsified: Height(1) =!= Height(1)")
+
+  property("(h1 compare h2) == 0") =
+    test(Claim((h1 compare h2) == 0), "falsified: OrderForHeight.compare(Height(1), Height(2)) {-1} == 0")
+
+  property("(h1 partialCompare h2) == 0.0") =
+    test(Claim((h1 partialCompare h2) == 0.0), "falsified: OrderForHeight.partialCompare(Height(1), Height(2)) {-1.0} == 0.0")
+
+  property("h1 < h1") =
+    test(Claim(h1 < h1), "falsified: Height(1) < Height(1)")
+
+  property("h2 <= h1") =
+    test(Claim(h2 <= h1), "falsified: Height(2) <= Height(1)")
+
+  property("h1 > h1") =
+    test(Claim(h1 > h1), "falsified: Height(1) > Height(1)")
+
+  property("h1 >= h2") =
+    test(Claim(h1 >= h2), "falsified: Height(1) >= Height(2)")
+
+  property("(1 pmin 2) = None") =
+    test(Claim((1 pmin 2) == None), "falsified: 1 pmin 2 {Some(1)} == None")
+
+  property("(1 pmax 2) = None") =
+    test(Claim((1 pmax 2) == None), "falsified: 1 pmax 2 {Some(2)} == None")
+
+  property("(1 min 2) = None") =
+    test(Claim((1 min 2) == 0), "falsified: 1 min 2 {1} == 0")
 
   property("(1 max 2) = 0") =
     test(Claim((1 max 2) == 0), "falsified: 1 max 2 {2} == 0")
 
   property("(1 min 2) = 0") =
     test(Claim((1 min 2) == 0), "falsified: 1 min 2 {1} == 0")
+
+  property("List(1,2,3).size === 4") =
+    test(Claim(List(1,2,3).size === 4), "falsified: List(1, 2, 3).size {3} === 4")
 }
